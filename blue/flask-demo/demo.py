618170f838f9f8c83fbd7e862def93d639cf37f0
saleshc <48942588+saleshc@users.noreply.github.com>
2024-03-03 09:59:27 +0530

    demo.py

--- blue/flask-demo/demo.py
+++ blue/flask-demo/demo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/') 
 def hello(): 
-    return "welcome to the flask tutorials updated"
+    return "welcome to the flask tutorials updated /n This is edited by Salesh"
   
   
 if __name__ == "__main__": 
